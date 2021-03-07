c80c53e5684082558361a080af1a2395a3dc50f2
THIO ALLI <33598841+aallithioo@users.noreply.github.com>
2021-03-07 12:43:37 +0700

    Update PreventWappalyzer.js

--- PreventWappalyzer.js
+++ PreventWappalyzer.js
@@ -1,12 +1,13 @@
-// flushFile = document.removeChild("script");
+flushFile = document.removeChild("Wappalyzer");
 // flushFile.setAttribute("src", chrome.extension.getURL('js/inject.js'));
 // document.body.appendChild(flushFile);
 
 //Loads flush.js into the DOM tree, overwriting all functions and variables of main.js in browser memory to null
 
 document.body.remove('content.js');
-// document.body.remove('inject.js');
+document.body.remove('inject.js');
 document.body.remove(chrome.extension.getURL('js/inject.js'));
 document.setAttribute.remove(chrome.extension.getURL('js/inject.js'))
+document.setAttribute.remove(chrome.extension.getURL('js/content.js'))
 //  script.setAttribute('src', chrome.extension.getURL('js/inject.js')
 document.remove.getURL('js/inject')
